06287142d6ffdd254267f3cb52d64ebd7f0e4472
zsuetam123 <60672275+zsuetam123@users.noreply.github.com>
2021-03-25 23:31:09 +0100

    Add files via upload

---
+++ tworzenie_bazy_danych/src/main/java/technologie_obiektowe/tworzenie_bazy_danych/TworzenieBazyDanychApplication.java
@@ -0,0 +1,13 @@
+package technologie_obiektowe.tworzenie_bazy_danych;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TworzenieBazyDanychApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TworzenieBazyDanychApplication.class, args);
+	}
+
+}
---
+++ tworzenie_bazy_danych/src/test/java/technologie_obiektowe/tworzenie_bazy_danych/TworzenieBazyDanychApplicationTests.java
@@ -0,0 +1,13 @@
+package technologie_obiektowe.tworzenie_bazy_danych;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TworzenieBazyDanychApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
