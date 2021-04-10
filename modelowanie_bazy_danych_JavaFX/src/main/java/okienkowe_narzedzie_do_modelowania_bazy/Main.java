992befe8cc35e5999c0b7190b6625cd684212583
zsuetam123 <60672275+zsuetam123@users.noreply.github.com>
2021-04-10 13:41:50 +0200

    Add files via upload

---
+++ modelowanie_bazy_danych_JavaFX/src/main/java/okienkowe_narzedzie_do_modelowania_bazy/Main.java
@@ -0,0 +1,17 @@
+package okienkowe_narzedzie_do_modelowania_bazy;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+        primaryStage.setTitle("Narzędzie do modelowania bazy danych");
+        primaryStage.show();
+    }
+}
